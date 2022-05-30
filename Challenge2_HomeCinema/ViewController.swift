3a5a16dce1c7fd32a56e2eca248fac272c3f164b
Daniil Kiryanchuk <job.kiryanchuk@gmail.com>
2022-05-30 10:07:25 +0300

    Initial Commit

---
+++ Challenge2_HomeCinema/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Challenge2_HomeCinema
+//
+//  Created by Даниил Кирьянчук on 30.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
