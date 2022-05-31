610190ad0c819a8a1e9f2722085bac889049dcb2
Daniil Kiryanchuk <job.kiryanchuk@gmail.com>
2022-05-31 14:05:19 +0300

    Создал ActorsCollectionView, SecondViewController. Также перенес старт проекта со второго экрана чтобы видеть изменения

--- Challenge2_HomeCinema/SecondViewController.swift
+++ Challenge2_HomeCinema/SecondViewController.swift
@@ -9,4 +9,20 @@ import UIKit
 
 class SecondViewController: UIViewController {
     
+    
+}
+
+extension SecondViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // количество рядов у таблицы
+        return 3
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        //создаем ячейку
+
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell", for: indexPath) as! TableViewCell
+
+       return cell
+}
 }
