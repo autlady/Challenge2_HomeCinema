9151b37753c23d9ba45060ab5c2eceb61cd6fd75
Daniil Kiryanchuk <job.kiryanchuk@gmail.com>
2022-05-31 20:17:14 +0300

    Привязал "backButtonTapped", "movieTitle", "MovieOverView"  к коду

--- Challenge2_HomeCinema/SecondViewController.swift
+++ Challenge2_HomeCinema/SecondViewController.swift
@@ -10,30 +10,85 @@ import UIKit
 class SecondViewController: UIViewController {
     
     
+
+    
     @IBOutlet weak var actorCollectionView: UICollectionView!
+    
+    @IBOutlet weak var movieTitle: UILabel!
+    
+    @IBOutlet weak var movieOverview: UITextView!
+    
+    @IBAction func backButtonTapped(_ sender: UIButton) {
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        view.backgroundColor = .black
+        //        view.backgroundColor = .black
         actorCollectionView.dataSource = self
         actorCollectionView.delegate = self
         actorCollectionView.register(ActorsCollectionViewCell.self, forCellWithReuseIdentifier: "ActorsCollectionViewCell") //регистрирую ячейку
         
     }
+    
+    lazy var imageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "film"))
+        imageView.layer.borderWidth = 3.0
+        imageView.layer.borderColor = UIColor.white.cgColor
+        imageView.layer.cornerRadius = 70
+        imageView.clipsToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    lazy var labelStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.spacing = 40
+        return stackView
+    }()
+    
+    lazy var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.spacing = 16
+        return stackView
+    }()
+    
+    lazy var fullNameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "filmNumber1"
+        label.textColor = .black
+        label.font = UIFont(name: "Helvetica-Bold", size: 18)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    
+    func drawSelf() {
+        self.stackView.addArrangedSubview(imageView)
+        self.stackView.addArrangedSubview(fullNameLabel)
+    }
 }
+
+
+    // MARK: - extensions
+
 
 extension SecondViewController: UICollectionViewDelegateFlowLayout {
     private var sideInset: CGFloat { return 8 }
-
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-
+        
         // настраиваю размер ячеек
         let width = (collectionView.bounds.width - sideInset * 3) / 4
         return CGSize(width: width, height: width)
     }
-
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-
+        
         // настраиваю отступы между ячейками
         sideInset
     }
@@ -41,15 +96,15 @@ extension SecondViewController: UICollectionViewDelegateFlowLayout {
 
 extension SecondViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-
+        
         // количество ячеек
-        12
+        6
     }
-
+    
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-
+        
         // создаю ячейку
-
+        
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ActorsCollectionViewCell", for: indexPath) as! ActorsCollectionViewCell
         cell.setupCell()
         return cell
