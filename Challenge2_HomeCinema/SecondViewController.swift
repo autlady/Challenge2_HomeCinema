d947ac58f021ecbefd3312e55f6966b71a178546
Daniil Kiryanchuk <job.kiryanchuk@gmail.com>
2022-05-31 13:07:31 +0300

    Создал в main.storyboard новый view и добавил во view элементы, согласно нашему макету

---
+++ Challenge2_HomeCinema/SecondViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SecondViewController.swift
+//  Challenge2_HomeCinema
+//
+//  Created by Даниил Кирьянчук on 31.05.2022.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+}
